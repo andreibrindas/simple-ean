f06fc45af1fdd25963d9013daf5e85b944d01e35
andreibrindas <31765195+andreibrindas@users.noreply.github.com>
2021-03-22 21:55:46 +0200

    first commit

---
+++ includes/ean.php
@@ -0,0 +1,49 @@
+<?php
+
+// EAN field variable
+add_action( 'woocommerce_product_after_variable_attributes', 'variation_settings_fields', 10, 3 );
+add_action( 'woocommerce_save_product_variation', 'save_variation_settings_fields', 10, 2 );
+
+function variation_settings_fields( $loop, $variation_data, $variation ) {
+    woocommerce_wp_text_input(
+        array(
+            'id'            => "cod_ean{$loop}",
+            'name'          => "cod_ean[{$loop}]",
+            'value'         => get_post_meta( $variation->ID, 'cod_ean', true ),
+            'label'         => __( 'EAN', 'woocommerce' ),
+            'wrapper_class' => 'form-row form-row-full',
+        )
+    );
+}
+
+function save_variation_settings_fields( $variation_id, $loop ) {
+    $text_field = $_POST['cod_ean'][ $loop ];
+
+    if ( ! empty( $text_field ) ) {
+        update_post_meta( $variation_id, 'cod_ean', esc_attr( $text_field ));
+    }
+}
+
+// EAN field simple
+add_action('woocommerce_product_options_inventory_product_data', 'product_inventory_data', 10, 0 );
+add_action('woocommerce_process_product_meta', 'save_product_meta', 10, 2 );
+
+
+function product_inventory_data() {
+    woocommerce_wp_text_input(
+        array(
+            'id'            => "cod_ean",
+            'name'          => "cod_ean",
+            'value'         => get_post_meta( get_the_ID(), 'cod_ean', true ),
+            'label'         => __( 'EAN', 'woocommerce' ),
+        )
+    );
+}
+
+function save_product_meta($id, $post) {
+    $text_field = $_POST['cod_ean'];
+
+    if ( ! empty( $text_field ) ) {
+        update_post_meta( $id, 'cod_ean', esc_attr( $text_field ));
+    }
+}
---
+++ simple-ean.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Name
+ *
+ * @package           Simple Woocommerce EAN
+ * @author            Andrei Brindas
+ * @copyright         2021 Andrei Brindas
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Simple Woocommerce EAN
+ * Plugin URI:        https://andreibrindas.com/simple-ean
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Andrei Brindas
+ * Author URI:        https://andreibrindas.com
+ * Text Domain:       simple-ean
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+function simple_ean_check_for_woo() {
+    $active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins' ) );
+    if ( in_array( 'woocommerce/woocommerce.php', $active_plugins ) ) {
+        // Plugin is active
+        require_once( "./includes/ean.php" );
+    } else {
+        ?>
+            <div class="updated notice is-dismissible">
+                <p>You can't activate this plugin unles you have <strong>Woocommerce active</strong>. Please activate Woocommerce before trying to activate this plugin.</p>
+            </div>
+        <?php
+    }
+}
+
+
+
+function simple_ean_activate() { 
+    // Trigger our function that registers the custom post type plugin.
+    simple_ean_check_for_woo();
+}
+register_activation_hook( __FILE__, 'simple_ean_activate' );
